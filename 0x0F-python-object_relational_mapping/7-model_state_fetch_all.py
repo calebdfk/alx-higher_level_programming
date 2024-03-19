b2ad229a7b73d94c287fc0c21f4bfc3808317a94
calebdfk <dkweteza@outlook.com>
2024-03-19 03:15:11 -0700

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
 
-""" Write a script that lists all State objects """
-
 if __name__ == "__main__":
 
-    from sqlalchemy import create_engine
-    from sqlalchemy.ext.declarative import declarative_base
-    from sqlalchemy.orm import sessionmaker
     import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
     from model_state import Base, State
 
-    inp = sys.argv
-    if len(inp) < 4:
-        exit(1)
-    conn_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-    engine = create_engine(conn_str.format(inp[1], inp[2], inp[3]))
-    Session = sessionmaker(bind=engine)
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    Base.metadata.create_all(engine)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    session = Session()
+    conn_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database)
 
-    output = session.query(State).order_by(State.id).all()
-    for state in output:
-        print("{}: {}".format(state.id, state.name))
+    try:
+        engine = create_engine(conn_str)
+        Session = sessionmaker(bind=engine)
+        session = Session()
 
-    session.close()
+        Base.metadata.create_all(engine)
+
+        states = session.query(State).order_by(State.id).all()
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+
+    except Exception as e:
+        print("Error:", e)
+        sys.exit(1)
+
+    finally:
+        if session:
+            session.close()
