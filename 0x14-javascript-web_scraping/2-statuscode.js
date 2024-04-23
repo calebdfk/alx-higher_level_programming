f0c331cf1037837b4fc3e516b561ebf309d21209
calebdfk <dkweteza@outlook.com>
2024-04-23 10:43:49 -0700

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,16 +2,12 @@
 
 const request = require('request');
 
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const url = process.argv[2];
 
-request.get(url, (error, response, body) => {
+request.get(url, (error, response) => {
   if (error) {
     console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
   } else {
-    const film = JSON.parse(body);
-    console.log(film.title);
+    console.log(`code: ${response.statusCode}`);
   }
 });
